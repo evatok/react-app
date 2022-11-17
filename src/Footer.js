1b3a7059816cf07f79e9e20583d092dcb2250bbf
evatok <teo@ukr.net>
2022-11-17 20:39:59 +0100

    rel="noreferrer" added

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Footer(){
     return (<div className="Footer">
         <div className="container mt-5">
-        <a href="https://github.com/evatok/react-app" target="_blank">Open-source code by Eva Tokarchuk</a>
+        <a href="https://github.com/evatok/react-app" target="_blank" rel="noreferrer">Open-source code by Eva Tokarchuk</a>
         </div>
         </div>)
 }
